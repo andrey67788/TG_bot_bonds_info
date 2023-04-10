8899569da720cb4a3ae411a7be2d206db38733eb
Andrey Sokolovskiy <andrey67788@gmail.com>
2023-04-10 21:40:06 +0300

    add duration count for Coupon_Bonds

--- Class_Bond.py
+++ Class_Bond.py
@@ -2,8 +2,17 @@ import numpy as np
 
 
 class Coupon_Bonds():
-    def YTM():
+
+    def YTM(self):
         pass
 
-    def duration_calc():
-        pass
+    def duration_count(self, c, y, m, n):
+        macaulay_duration = ((1 + y) / (m * y)) - ((1 + y + n * (c - y)) / ((m * c * ((1 + y) ** n - 1)) + m * y))
+        modified_duration = macaulay_duration / (1 + y)
+
+        return macaulay_duration, modified_duration
+
+
+my_bond = Coupon_Bonds()
+duration = my_bond.duration_count(c=0.035, y=.04, m=2, n=6)
+print(duration)
