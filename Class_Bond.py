280f32c6d89a2cda1af78535c0f090643cc360e7
Andrey Sokolovskiy <andrey67788@gmail.com>
2023-04-13 12:55:28 +0300

    added name changes

--- Class_Bond.py
+++ Class_Bond.py
@@ -1,18 +1,16 @@
-import numpy as np
+class CouponBonds:
 
-
-class Coupon_Bonds():
-
-    def YTM(self):
+    def ytm(self):
         pass
 
-    def duration_count(self, c, y, m, n):
+    @staticmethod
+    def duration_count(c, y, m, n):
         macaulay_duration = ((1 + y) / (m * y)) - ((1 + y + n * (c - y)) / ((m * c * ((1 + y) ** n - 1)) + m * y))
         modified_duration = macaulay_duration / (1 + y)
 
         return macaulay_duration, modified_duration
 
 
-my_bond = Coupon_Bonds()
+my_bond = CouponBonds
 duration = my_bond.duration_count(c=0.035, y=.04, m=2, n=6)
 print(duration)
